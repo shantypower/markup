bffb361fefef1262d7bccd0284f0cfc602fcedd0
shantypower <shantypower4891@yandex.ru>
2018-09-27 15:48:03 +0900

    оживляет слайдер

--- markup/src/assets/js/app.js
+++ markup/src/assets/js/app.js
@@ -1,96 +1,53 @@
-import $ from 'jquery';
-import whatInput from 'what-input';
+var slides = document.querySelectorAll('.reviews__list .reviews__list-item');
+var currentSlide = 0;
+//var slideInterval = setInterval(nextSlide, 2000);
 
-window.$ = $;
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
 
-import Foundation from 'foundation-sites';
-// If you want to pick and choose which modules to include, comment out the above and uncomment
-// the line below
-//import './lib/foundation-explicit-pieces';
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
 
-import 'tablesaw/dist/tablesaw.jquery';
-import libs from './lib/dependancies';
-window.libs = libs;
+function goToSlide(n) {
+  slides[currentSlide].className = 'reviews__list-item';
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].className = 'reviews__list-item showing';
+}
 
-$(document).foundation();
+/*
+var playing = true;
+var pauseButton = document.getElementById('pause');
 
-libs.AOS.init();
+function pauseSlideshow() {
+  pauseButton.innerHTML = 'Play';
+  playing = false;
+  clearInterval(slideInterval);
+}
 
-// SVG Injector
-// Elements to inject
-var mySVGsToInject = document.querySelectorAll('img.inject-me');
+function playSlideshow() {
+  pauseButton.innerHTML = 'Pause';
+  playing = true;
+  slideInterval = setInterval(nextSlide, 2000);
+}
 
-// Options
-var injectorOptions = {
-  evalScripts: 'once',
-  pngFallback: 'assets/png'
+pauseButton.onclick = function () {
+  if (playing) {
+    pauseSlideshow();
+  } else {
+    playSlideshow();
+  }
+};*/
+
+var next = document.querySelector('.reviews__arrow--right');
+var previous = document.querySelector('.reviews__arrow--left');
+
+next.onclick = function () {
+  //pauseSlideshow();
+  nextSlide();
 };
-
-var afterAllInjectionsFinishedCallback = function (totalSVGsInjected) {
-  // Callback after all SVGs are injected
-  console.log('We injected ' + totalSVGsInjected + ' SVG(s)!');
+previous.onclick = function () {
+  //pauseSlideshow();
+  previousSlide();
 };
-
-var perInjectionCallback = function (svg) {
-  // Callback after each SVG is injected
-  console.log('SVG injected: ' + svg);
-};
-
-// create injector configured by options
-var injector = new libs.svgInjector(injectorOptions);
-
-// Trigger the injection
-injector.inject(
-  mySVGsToInject,
-  afterAllInjectionsFinishedCallback,
-  perInjectionCallback
-);
-
-// slick carousel
-$(".content-carousel").slick({
-  // normal options...
-  speed: 5000,
-	autoplay: true,
-	autoplaySpeed: 0,
-	cssEase: 'linear',
-  slidesToShow: 5,
-	slidesToScroll: 1,
-  infinite: true,
-  swipeToSlide: true,
-	centerMode: true,
-  focusOnSelect: true,
-  // the magic
-  responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        infinite: true
-      }
-    }, {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        dots: true
-      }
-    }, {
-      breakpoint: 300,
-      settings: "unslick" // destroys slick
-    }]
-});
-
-// tablesaw table plugin
-$(function () {
-  $(document)
-    .foundation()
-    .trigger('enhance.tablesaw');
-});
-
-var TablesawConfig = {
-  swipeHorizontalThreshold: 15
-};
-
-// app dashboard toggle
-$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
-  e.preventDefault();
-  $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
-});
